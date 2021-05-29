b55d3dec54d4701ca534f8d32c0947b7df7225c5
shanicka-minor <67990192+shanicka-minor@users.noreply.github.com>
2021-05-30 01:11:59 +0800

    None, but pls pull

--- employeeChat.php
+++ employeeChat.php
@@ -4,7 +4,7 @@
 	{
 		header("location:login.php");
 	}
-	
+
 	else {
 ?>
 
@@ -36,13 +36,13 @@ Enter Message: <input type="text" name="txtMessage" size="20">
   $port = 50001;
   set_time_limit(0);
   if (isset($_POST["btnSend"])){
-	  
+
 	  $msg = $_REQUEST["txtMessage"];
 	  $sock = socket_create(AF_INET, SOCK_STREAM, 0);
 	  socket_connect($sock, $host, $port);
-	  
+
 	  socket_write($sock, $msg, strlen($msg));
-	  
+
 	  $reply = socket_read($sock, 1924);
 	  $reply = trim($reply);
 	  $reply = "Server says:\t". $reply;
