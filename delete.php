becea6c72eeedc90fdaf82b3a71e14ca0cf2d111
shanicka-minor <67990192+shanicka-minor@users.noreply.github.com>
2021-05-30 04:09:59 +0800

    Session destroyed when deleting account

--- delete.php
+++ delete.php
@@ -4,37 +4,41 @@
 	{
 		header("location:login.php");
 	}
-	
+
 	else {
 ?>
 
 <html>
-<head><title>Delete Module</title>
+<head><title>Bye!</title>
 <script type="text/javascript">
   function confirmation() {
-    alert ("Confirm Modification of Record?")
+    alert ("Confirm Deletion of Record?")
   }
 </script>
 </head>
 <body>
-<h1>Delete Module</h1>
 <hr>
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
   session_start();
   require("connect.php");
   if(isset($_SESSION['getID'])) {
-  
+
   $eid = $_SESSION['getID'];
-  
+
   $query = mysqli_query($DBConnect, "DELETE  FROM tblemployee WHERE empid='$eid'");
   $cell = mysqli_fetch_array($query);
      echo "Record has been deleted";
   }
  ?>
 
-<a href='logout.php'>View All</a>
 </body>
 </html>
 
 <?php } ?>
+
+<?php
+session_start();
+session_destroy();
+header("location:login.php");
+?>
