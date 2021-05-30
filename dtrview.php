551b3da6e42ab35a04598be3104e5ddc32d89e65
Oninolan <61404230+Oninolan@users.noreply.github.com>
2021-05-30 23:39:33 +0800

    dtrview design

--- dtrview.php
+++ dtrview.php
@@ -1,5 +1,8 @@
 <?php
 	session_start();
+	error_reporting(E_ALL ^ E_NOTICE);
+	error_reporting(0);
+	ini_set('display_errors', 0);
 	if(!isset($_SESSION["getLogin"]))
 	{
 		header("location:login.php");
@@ -13,23 +16,27 @@
 <head>
 <meta charset = "UTF-8">
 <title> Date of Efficiency </title>
+<link rel="stylesheet" href="style.css">
+<style>
+	body{
+        background: #554ca8;
+    }
+</style>
 </head>
 
 <body>
-<?php
-	error_reporting(E_ALL ^ E_NOTICE);
-	echo "Hi". ucwords($_SESSION["getLogin"]);
-?>
-
-&nbsp; &nbsp;<a href= "employeeChat.php">Chat </a>
-
-&nbsp; &nbsp;<a href= "modify.php">View Account </a>
-
-&nbsp; &nbsp;<a href= "logout.php">Logout </a>
-
-
+<ul>
+	<?php
+		error_reporting(E_ALL ^ E_NOTICE);
+		echo "<li style='padding: 14px 16px; float:left'>Hi ". ucwords($_SESSION["getLogin"])."</li>";
+	?>
+	<li><a href= "logout.php">Logout </a></li>
+	<li><a href= "employeeChat.php">Chat </a></li>
+	<li><a href= "edit.php">Edit Profile</a></li>
+</ul>
+<div><center>
 </b>
-<table border = "1" width="50%">
+<table border = "3" bordercolor="white" width="50%" frame=void rules=rows color=black style="margin:20px">
 		
 <?php
 	$dayArray = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
@@ -39,7 +46,7 @@
 	$SelectedSched=array();
 	$Rest=array();
 	
-	echo "<p><b>Date of Effectivity: </b>";
+	echo "<div style='color:white'><p><b>Date of Effectivity: </b>";
 	echo $startDate;
 	echo " | ";
 	
@@ -71,15 +78,15 @@
 	
 	if($Days<=5 && $endDate>=$startDate)
 	{
-		echo "<td> Day Present </td>";
-		echo "<td> Time in </td>";
-		echo "<td> Time out </td>";
-		echo "<td> Hours per day </td>";
+		echo "<td style='color:white'> Day Present </td>";
+		echo "<td style='color:white'> Time in </td>";
+		echo "<td style='color:white'> Time out </td>";
+		echo "<td style='color:white'> Hours per day </td>";
 		
 		
 		for($rows=1; $rows<=$Days; $rows++)
 		{
-			echo"<tr>";
+			echo"<tr style='color:white'>";
 			
 			$isOvertime="No";
 			$isUndertime="No";
@@ -112,31 +119,30 @@
 			
 			if ($HoursPerDay<6)
 			{
-				echo $SelectedDay ;
-				echo " did not meet the minimum hour.";
+				echo "<div style='color:#332d69'>".$SelectedDay ;
+				echo " did not meet the minimum hour.</div>";
 				$isUndertime="Yes";
 			}
-			
 			
 			echo "</tr>";
 		}
 		
 			echo "<tr>";
-			echo "<td colspan=3> Total Rendered Hours:   </td>";
-			echo "<td> $Total </td>";
+			echo "<td colspan=3 style='color:white'> Total Rendered Hours:   </td>";
+			echo "<td style='color:white'> $Total </td>";
 			echo "</tr>";
 			
 			echo "<tr>";		
-			echo "<td colspan=3> Overtime:   </td>";
-			echo "<td> $Overtime </td>";
+			echo "<td colspan=3 style='color:white'> Overtime:   </td>";
+			echo "<td style='color:white'> $Overtime </td>";
 			echo "</tr>";
 			
 			echo "<tr>";		
-			echo "<td colspan=3> Rest Day:   </td>";
+			echo "<td colspan=3 style='color:white'> Rest Day:   </td>";
 			
 			
 			
-			echo "<td> $restday </td>";
+			echo "<td style='color:white'> $restday </td>";
 			echo "</tr>";
 	}
 	
@@ -144,11 +150,14 @@
 	{
 		echo "Error, Please enter the proper amount of work days";
 	}
+	echo"</div>";
 	
-	
-  echo "<a href='dtr.php'>Back to DTR page | |</a>"; 
 ?>
+</center>
 </table>
+<br>
+<a href='dtr.php' style='text-decoration:none' class="button">Back to DTR page</a>
+</div>
 </body>
 </html>
 
