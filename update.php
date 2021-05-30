f1a750d40d4044e3da4382fbbc3e445e542c81ec
Oninolan <61404230+Oninolan@users.noreply.github.com>
2021-05-30 11:49:45 +0800

    update page design

--- update.php
+++ update.php
@@ -1,4 +1,7 @@
-<h3>Record Modification Confirmation</h3>
+<link rel="stylesheet" href="style.css">
+<div class = "logincentered"  style='border: 2px solid white; padding: 30px;border-radius: 15px;'>
+<h1>Record Modification Confirmation</h1>
+
 <?php
 	error_reporting(E_ALL ^ E_NOTICE);
   require("connect.php");
@@ -10,23 +13,23 @@
   $newGender = $_POST["gender"];
   $newUser = $_POST["user"];
   $newPass = $_POST["pass"];
-  
  
   mysqli_query($DBConnect, "UPDATE tblemployee SET empname='$newName', empstatus='$newStat', 
             empgender='$newGender', empuser='$newUser', emppass='$newPass' WHERE empid='$newID'") or die (mysqli_error());
   
-  echo "<h3>Record has been saved. Please check the modification below.</h3>";
+  echo "<h3 style='color:white'>Record has been saved. Please check the modification below.</h3>";
   
   $query = mysqli_query($DBConnect, "SELECT * FROM tblemployee WHERE empid='$newID'");
   $fetch = mysqli_fetch_array($query);
-  echo "Employee Name : ". $fetch["empname"]. "<br />";
+  echo "<p style='color:white'>Employee Name : ". $fetch["empname"]. "<br />";
   echo "Employee Status : ". $fetch["empstatus"]. "<br />";
   echo "Employee Gender : ". $fetch["empgender"]. "<br />";
   echo "Employee Username : ". $fetch["empuser"]. "<br />";
-  echo "Employee Password : ". $fetch["emppass"]. "<br />";
+  echo "Employee Password : ". $fetch["emppass"]. "<br /></p>";
   
  }
 
 ?>
 <br />
-<a href="dtr.php">Back</a>  
+<center><a style='text-decoration:none' href="dtr.php" class="button">Back</a>  </center>
+</div>
